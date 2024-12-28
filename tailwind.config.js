6f571ce26e5d7e1213f50e1c208ea0d4eb2f9db4
Ryohskay <r.nagata.21@abdn.ac.uk>
2024-12-28 13:39:47 +0900

    initial commit with minimal templating tools

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.[md,html,markdown]", 
+    "./_layouts/*.html", 
+    "./_includes/*.html", 
+    "./_posts/*.[md,html]",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
