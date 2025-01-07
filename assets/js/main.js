7eac4461f98726c05325f1fc519d6fd702d8fb09
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-01-07 20:35:24 +0900

    update header button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
 function dropdown() {
-    let mobileNav = document.querySelector("#mobile_nav");
-    let menuButton = document.querySelector("#hamburger")
+    let mobileNav = document.getElementById("mobile_nav");
+    let menuButton = document.getElementById("menu_sec");
+    let menuClose = document.getElementById("menu_close");
 
-    mobileNav.classList.toggle("hidden")
-    mobileNav.classList.toggle("flex");
-    mobileNav.classList.toggle("flex-col");
-    menuButton.classList.toggle("rotate-90");
+    menuButton.classList.remove("flex");
+    menuButton.classList.add("hidden");
+
+    mobileNav.classList.remove("hidden");
+    mobileNav.classList.add("flex");
+    mobileNav.classList.add("flex-col");
+
+    menuClose.classList.remove("hidden");
+    menuClose.classList.add("flex");
 };
+
+function closePane() {
+    let mobileNav = document.getElementById("mobile_nav");
+    let menuButton = document.getElementById("menu_sec");
+    let menuClose = document.getElementById("menu_close");
+
+    menuButton.classList.remove("hidden");
+    menuButton.classList.add("flex");
+
+    mobileNav.classList.remove("flex");
+    mobileNav.classList.remove("flex-col");
+    mobileNav.classList.add("hidden");
+
+    menuClose.classList.remove("flex");
+    menuClose.classList.add("hidden");
+}
